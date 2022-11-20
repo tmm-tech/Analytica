ec84684ba401d25986159c1d021a33ac69b9f86c
tonym <tonymugi074@gmail.com>
2022-11-20 22:09:00 +0300

    first comment

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ from prediction import predict_value
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
     print(__name__)
     return 'Analytica'
 
 
-@app.route('/prediction/<stock>', methods=['POST'])
+@app.route('/prediction/<stock>', methods=['POST', 'GET'])
 def predict(stock):
     try:
         result = predict_value(stock)
         return result
     except EOFError as e:
-           print(e)
+        print("as: " + e)
 
 
 if __name__ == '__main__':
